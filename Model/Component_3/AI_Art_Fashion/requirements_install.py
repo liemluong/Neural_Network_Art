660ce2690be141f1e8453bf0a2e326587940e795
liemluong <liemluong@outlook.com>
2022-06-19 15:40:18 -0700

    Add files for segmentation and masking component

---
+++ Model/Component_3/AI_Art_Fashion/requirements_install.py
@@ -0,0 +1,5 @@
+# Make sure to install these Python packages/libraries first (run them with admin account)
+pip install torchvision
+pip install opencv-python
+pip install iglovikov_helper_functions
+pip install cloths_segmentation
