58e03215b8b26b3ca8ee1410c38f1c7ba6d1d60b
liemluong <liemluong@outlook.com>
2022-06-19 15:43:18 -0700

    Delete Model/Component_2/AI_Art_Fashion directory

--- Model/Component_2/AI_Art_Fashion/requirements_install.py
+++
@@ -1,5 +0,0 @@
-# Make sure to install these Python packages/libraries first (run them with admin account)
-pip install torchvision
-pip install opencv-python
-pip install iglovikov_helper_functions
-pip install cloths_segmentation
